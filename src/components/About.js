0ad1b5dd05196d82fb8c932eeb9ddca478689949
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-03 19:26:49 +0100

    components added

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+export default function About() {
+  return (
+    <h1>About Page</h1>
+  )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <h1>Home Page</h1>
+  )
+}
