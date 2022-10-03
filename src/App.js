5cfc8c5cd4e9d202cccd8c24ebbb85fc3166ef0a
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-03 19:29:44 +0100

    Home component imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./style.css";
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 
 export default function App() {
   return (
+    <>
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      </Routes>
+      </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 
 export default function Home() {
