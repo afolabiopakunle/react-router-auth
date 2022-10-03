415fdcb87086923a25d65452d651e226e818f74c
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-03 19:40:54 +0100

    rewards page implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./style.css";
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Rewards from './components/Rewards';
 
 export default function App() {
   return (
@@ -11,12 +12,15 @@ export default function App() {
      <nav>
        <ul>
          <li><Link to='/'>Home</Link></li>
+         <li><Link to='/rewards'>Rewards</Link></li>
          <li><Link to='/about'>About</Link></li>
          </ul>
      </nav>
     </div>
     <Routes>
       <Route path='/' element={<Home />} />
+      
+      <Route path='/rewards' element={<Rewards />} />
       <Route path='/about' element={<About />} />
       </Routes>
       </>
---
+++ src/components/Rewards.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Rewards() {
+  return (
+    <h1>Rewards Page</h1>
+  )
+}
