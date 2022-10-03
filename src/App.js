ef6bc84fd7c7888197999544209058cbc4002866
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-03 19:31:57 +0100

    about page implemented

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./style.css";
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
 
 export default function App() {
   return (
     <>
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+     <nav>
+       <ul>
+         <li><Link to='/'>Home</Link></li>
+         <li><Link to='/about'>About</Link></li>
+         </ul>
+     </nav>
     </div>
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
       </Routes>
       </>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Routes, Route, Link } from 'react-router-dom';
 
 export default function About() {
